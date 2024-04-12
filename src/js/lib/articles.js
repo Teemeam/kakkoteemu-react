7011cbb1553892d72e0e03cc5c7f33f478d61225
Teemu Kakko <sormiteippi@windowslive.com>
2024-04-12 13:44:09 +0300

    Update articles.js

--- src/js/lib/articles.js
+++ src/js/lib/articles.js
@@ -769,4 +769,13 @@ export const articles = [
     publisher: 'Yle',
     role: 'code',
   },
+  {
+    type: 'web',
+    title: 'Suomen hallitus haluaa säästää kolme miljardia euroa ja äkkiä – leikkaa itse ja päätä, kuka kärsii',
+    url: 'https://yle.fi/a/74-20082596',
+    image_id: '39-12690406617b28c2ff6d',
+    published: '12.4.2024',
+    publisher: 'Yle',
+    role: 'code',
+  },
 ];
